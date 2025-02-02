79272878ce03a68d0e1bf53ea89efae5a5f242d8
preetam <preetamjutanatti06@gmail.com>
2025-02-02 18:23:58 +0530

    qwert

--- lab_assignment/lab03D1/rooting_for_you.cpp
+++ lab_assignment/lab03D1/rooting_for_you.cpp
@@ -1,20 +1,23 @@
 #include <simplecpp>
+#include <iomanip> 
 
 main_program{
-    float n; cin >> n;
-    float root = 0;
-    bool done = 1;
-    float middle = n/2;
-    float low = 0;
-    float high = n;
+    double n; cin >> n;
+    double low = 0;
+    double high = n;
+    double e = 0.000001;
 
+    while(high - low >= e){
+        double mid = (high + low)/2;
 
-    while(done){
-
-
-        
-
-
+        if(mid*mid > n){
+            high = mid;
+        }
+        else{
+            low = mid;
+        }
     }
-    cout << root;
+    cout <<fixed << setprecision(6) <<(high + low)/2;
+
+
 }
