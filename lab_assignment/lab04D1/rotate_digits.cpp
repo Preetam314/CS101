bc99821c5a283b7c913998a803d132bd68c50cf8
preetam <preetamjutanatti06@gmail.com>
2025-02-09 14:57:59 +0530

    lab4 D1

---
+++ lab_assignment/lab04D1/rotate_digits.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int rotateMe(int n, string c){
+    for(int i = 0; i < c.size())
+
+
+}
+
+int main(){
+    int n; cin >> n;
+    string c; cin >> c;
+
+    cout << rotateMe(n, c);
+
+}
---
+++ lab_assignment/lab04D1/temp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 1;
+    int y = 2;
+    int z = 0;
+
+    z = x & y;
+    cout << "hello world";
+    return 1344;
+
+}
